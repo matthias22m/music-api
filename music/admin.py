72d6b6101467939c1c58cf784c0448e04a1cd1f2
Matthias Mulugeta <matthias22m@gmail.com>
2024-03-21 20:27:29 +0300

    added: music, album, playlist model modified: settings for media directory

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Music,Album,Playlist
 
 # Register your models here.
+
+admin.site.register(Music)
+admin.site.register(Album)
+admin.site.register(Playlist)
--- music/models.py
+++ music/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+class Music(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    duration = models.DurationField()
+    audio_file = models.FileField(upload_to='musics')
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.PROTECT)
+    
+
+    def __str__(self):
+        return self.title
+    
+    
+class Playlist(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    songs = models.ManyToManyField(Music, related_name='playlists')
+
+    def __str__(self):
+        return self.title
+
