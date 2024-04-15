15fd2b9a5bc6c6d83aa5aafc36de525d65196d5b
Matthias Mulugeta <matthias22m@gmail.com>
2024-04-15 08:48:52 +0300

    modified: change some fields property

--- music/models.py
+++ music/models.py
@@ -3,12 +3,13 @@ from django.contrib.auth.models import User
 
 
 class Album(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     artist = models.CharField(max_length=100)
-    release_date = models.DateField()
+    release_date = models.DateField(auto_created=True)
 
     def __str__(self):
         return self.title
+
 
 class Music(models.Model):
     title = models.CharField(max_length=100)
@@ -16,19 +17,18 @@ class Music(models.Model):
     duration = models.DurationField()
     audio_file = models.FileField(upload_to='musics')
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    album = models.ForeignKey(Album, on_delete=models.PROTECT)
-    
+    album = models.ForeignKey(
+        Album, blank=True, null=True, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
-    
-    
+
+
 class Playlist(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    songs = models.ManyToManyField(Music, related_name='playlists')
+    songs = models.ManyToManyField(Music, related_name='playlist_songs')
 
     def __str__(self):
         return self.title
-
