7b02fe745619c98fab912af798456c6703eb43ff
Matthias Mulugeta <matthias22m@gmail.com>
2024-05-11 16:02:15 +0300

    modified: added cors allowed origin

--- MusicApp/settings.py
+++ MusicApp/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOWED_ORIGIN = ['http://127.0.0.1:5500','http://localhost:5500']
+
 
 # Application definition
 
@@ -40,11 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'music',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
