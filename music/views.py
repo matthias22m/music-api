d601a1bf3fb2aa0425ef5aada8fad51c74fc3cfe
Matthias Mulugeta <matthias22m@gmail.com>
2024-05-11 16:09:20 +0300

    modified: added all views and methods

--- music/views.py
+++ music/views.py
@@ -1,3 +1,105 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Music,Playlist ,Album
+from .serializers import MusicSerializer, AlbumSerializer, PlaylistSerializer
 
-# Create your views here.
+@api_view(['GET','POST'])
+def music_list(request):
+    if request.method == 'GET':
+        musics = Music.objects.all()
+        serializer = MusicSerializer(musics, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = MusicSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET','PUT', 'PATCH','DELETE'])
+def music_detail(request, id):
+    music = get_object_or_404(Music, pk=id)
+    if request.method == 'GET':
+        serializer = MusicSerializer(music)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = MusicSerializer(music,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PATCH':
+        serializer = MusicSerializer(music,data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        music.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET','POST'])
+def playlist_list(request):
+    if request.method == 'GET':
+        playlists = Playlist.objects.all()
+        serializer = PlaylistSerializer(playlists, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = PlaylistSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET','PUT','PATCH','DELETE'])
+def playlist_detail(request, id):
+    playlist = get_object_or_404(Playlist, pk=id)
+    if request.method == 'GET':
+        serializer = PlaylistSerializer(playlist)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = PlaylistSerializer(playlist,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PATCH':
+        serializer = PlaylistSerializer(playlist,data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        playlist.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET','POST'])
+def album_list(request):
+    if request.method == 'GET':
+        albums = Album.objects.all()
+        serializer = AlbumSerializer(albums, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = AlbumSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET','PUT','PATCH','DELETE'])
+def album_detail(request, id):
+    album = get_object_or_404(Album, pk=id)
+    if request.method == 'GET':
+        serializer = AlbumSerializer(album)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = AlbumSerializer(album,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PATCH':
+        serializer = AlbumSerializer(album,data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        if album.musics.count() > 0:
+            return Response({"error":"Album can not be deleted because it is associated with musics."},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        album.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
