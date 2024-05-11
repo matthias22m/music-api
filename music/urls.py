d1b29d251b1f20d46aa7b72417ddfb1441fde569
Matthias Mulugeta <matthias22m@gmail.com>
2024-05-11 16:08:13 +0300

    modified: add albums views url

--- music/urls.py
+++ music/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('musics/<int:id>/',views.music_detail),
     path('playlists/',views.playlist_list),
     path('playlists/<int:id>/',views.playlist_detail),
+    path('albums/',views.album_list),
+    path('albums/<int:id>/',views.album_detail),
 ]
